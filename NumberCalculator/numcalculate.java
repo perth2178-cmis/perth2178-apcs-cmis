5031b2375c2e6b7438b3afbe8274b33fe2fdf51a
cmis <cmis@cmis-hs10.(none)>
2015-08-19 11:52:16 +0700

    finished getting started
    .

--- NumberCalculator/numcalculate.java
+++ NumberCalculator/numcalculate.java
@@ -6,49 +6,51 @@
  * @version (a version number or a date)
  */
 import java.util.Scanner;
+import java.text.DecimalFormat;
 public class numcalculate
 {
   public static void main ( String[] args)
     {
+     DecimalFormat df = new DecimalFormat("#.000");
      int sum=0;
      Scanner user_input=new Scanner(System.in);
      String num1;
      System.out.print("Enter First Number ");
      num1=user_input.next();
-     int n1=Integer.parseInt(num1);
+     float n1=Integer.parseInt(num1);
      String num2;
      System.out.print("Enter Second Number ");
      num2=user_input.next();
-     int n2=Integer.parseInt(num2);
+     float n2=Integer.parseInt(num2);
      String result;
-     int s1=n1+n2;
-     int s2=n2+n1;
-     int s3=n1-n2;
-     int s4=n2-n1;
-     int s5=n1*n2;
-     int s6=n2*n1;
-     int s7=n1/n2;
-     int s8=n2/n1;
-     int s9=n1%n2;
-     int s10=n2%n1;
-     System.out.print(s1+" = "+n1+" + "+n2);
+     float s1=n1+n2;
+     float s2=n2+n1;
+     float s3=n1-n2;
+     float s4=n2-n1;
+     float s5=n1*n2;
+     float s6=n2*n1;
+     float s7=n1/n2;
+     float s8=n2/n1;
+     float s9=n1%n2;
+     float s10=n2%n1;
+     System.out.println(s1+" = "+n1+" + "+n2);
      
-     System.out.print(s2+" = "+n2+" + "+n1);
+     System.out.println(s2+" = "+n2+" + "+n1);
      
-     System.out.print(s3+" = "+n1+" - "+n2);
+     System.out.println(s3+" = "+n1+" - "+n2);
      
-     System.out.print(s4+" = "+n2+" - "+n1);
+     System.out.println(s4+" = "+n2+" - "+n1);
      
-     System.out.print(s5+" = "+n1+" * "+n2);
+     System.out.println(s5+" = "+n1+" * "+n2);
      
-     System.out.print(s6+" = "+n2+" * "+n1);
+     System.out.println(s6+" = "+n2+" * "+n1);
      
-     System.out.print(s7+" = "+n1+" / "+n2);
+     System.out.println(s7+" = "+n1+" / "+n2);
      
-     System.out.print(s8+" = "+n2+" / "+n1);
+     System.out.println(s8+" = "+n2+" / "+n1);
      
-     System.out.print(s9+" = "+n1+" % "+n2);
+     System.out.println(s9+" = "+n1+" % "+n2);
      
-     System.out.print(s10+" = "+n2+" % "+n1);
+     System.out.println(s10+" = "+n2+" % "+n1);
     }
 }
