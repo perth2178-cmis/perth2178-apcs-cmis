d243f6a210204236559f61eb9f6886003532c13a
cmis <cmis@cmis-hs09.(none)>
2015-08-21 13:14:06 +0700

    finished fundemental 1-3
    .

---
+++ Fundemental4/Fundemental4.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Fundemental4 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import javax.swing.JOptionPane;
+public class Fundemental4
+{
+    public static void main ( String args[])
+    {
+        int x = 1;
+      while( x < 11 ) {
+         System.out.print("value of x : " + x );
+         x++;
+         System.out.print("\n");
+      }
+    }
+}
---
+++ Template/Temp.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Temp here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import javax.swing.JOptionPane;
+public class Temp
+{
+    public static void main ( String args[])
+    {
+        String na=JOptionPane.showInputDialog("?");
+        System.out.println("?");
+    }
+}
