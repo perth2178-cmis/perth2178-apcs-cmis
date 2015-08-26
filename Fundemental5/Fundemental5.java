fe4fd056ca8cff731b028e6fe47781053dc8b226
cmis <cmis@cmis-hs09.(none)>
2015-08-26 14:28:26 +0700

    started fundemental6
    .

--- Fundemental5/Fundemental5.java
+++ Fundemental5/Fundemental5.java
@@ -11,8 +11,11 @@ public class Fundemental5
     public static void main ( String args[])
     {
         String loop;
-        for(int i=1; i<11; i++)
+        String a=JOptionPane.showInputDialog("Give me a number");
+        int x=Integer.parseInt(a);
+        for(int i=1; i<x; i++)
         {
+         System.out.println("\n");
          System.out.println("Jurassic Park"+"\n"+"Doctor Who 50th Anniversary"+"\n"+"Iron Man");
          System.out.println("Don't Stop Me Now"+"\t"+"Smooth Criminal"+"\t"+"Rhapsody in Blue");
         }
---
+++ Fundemental6/Fundemental6.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Fundemental6 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import javax.swing.JOptionPane;
+import java.util.Random;
+public class Fundemental6
+{
+    public static void main ( String args[])
+    {
+        int[] myArray;
+        myArray=new int[50];
+        for(int index=0; index<myArray.length; index++ )
+        {
+            System.out.println ((int) Math.ceil(Math.random()*100));
+        }
+    }
+}
