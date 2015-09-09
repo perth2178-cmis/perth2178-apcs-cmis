a752806685d2554c23546accc4558ce2f42d82da
cmis <cmis@cmis-hs09.(none)>
2015-09-09 09:57:24 +0700

    bigProble
    .

---
+++ BigProblem/Histagram.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+import java.util.Random;
+public class Histagram
+{
+    public static void main (String args[])
+    {
+        int [] Arraylist;
+        Arraylist = new int[100];
+        String input=JOptionPane.showInputDialog("Enter a number");
+        int num=Integer.parseInt(input);
+        int [] Arrayrandom;
+        Arrayrandom = new int[num];
+        for(int irandom=0;irandom<Arrayrandom.length;irandom++)
+        {
+           Arrayrandom[irandom]=(int) (100*(Math.random()));
+           
+                for(int index = 0;index<Arraylist.length;index++)
+              {
+                   System.out.print(index+":"); 
+                   if(index==Arrayrandom[irandom])
+                   {
+                       System.out.print("#");
+                       System.out.print("\n");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                        System.out.print("\n");
+                    }
+              }
+
+        }
+        
+    }
+}
