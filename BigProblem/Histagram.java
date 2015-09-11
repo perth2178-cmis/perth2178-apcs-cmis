99cff44cf7a9c329bca0451d7291744fb263a873
cmis <cmis@cmis-hs09.(none)>
2015-09-11 14:08:30 +0700

    big problem day 3
    .

--- BigProblem/Histagram.java
+++ BigProblem/Histagram.java
@@ -6,28 +6,33 @@ public class Histagram
     {
         int [] Arraylist;
         Arraylist = new int[100];
+        int [] Hashtag;
+        Hashtag= new int[100];
         String input=JOptionPane.showInputDialog("Enter a number");
         int num=Integer.parseInt(input);
         int [] Arrayrandom;
         Arrayrandom = new int[num];
+        int hashtag=0;
         for(int irandom=0;irandom<Arrayrandom.length;irandom++)
         {
            Arrayrandom[irandom]=(int) (100*(Math.random()));
+           int n= Arrayrandom[irandom];
+           for(int h=0;h<Hashtag.length; h++)
+           {
+            Hashtag[n]=Hashtag[n]++;
+           }
         }
+        
         for(int index = 0;index<Arraylist.length;index++)
         {
             System.out.print(index+":");
-            for(int irandom=0;irandom<Arrayrandom.length;irandom++)
+            if(Hashtag[n]>0)
             {
-                int i=0;
-              if(index==Arrayrandom[irandom])
-                 {
-                    System.out.print("#");  
-                 }
-              else
-                 {
-                     System.out.print(" ");  
-                 }
+                System.out.print("#");
+            }
+            else
+            {
+                System.out.print(" ");
             }
             System.out.print("\n");
         }
