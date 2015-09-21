e19f1a482748cfe53a1b84ae50ac11f55534b4a4
cmis <cmis@cmis-hs09.(none)>
2015-09-21 11:50:34 +0700

    finally done with big problem
    .

--- BigProblem/Histagram.java
+++ BigProblem/Histagram.java
@@ -10,10 +10,17 @@ public class Histagram
         int num=Integer.parseInt(input);
         int [] Arrayrandom;
         Arrayrandom = new int[num];
+        int max=0;
+        int min=99;
+        int sum=0;
+        double avg=0;
+        int moc=0;
+        int mst[]=new int [100];
         for(int irandom=0;irandom<Arrayrandom.length;irandom++)
         {
            Arrayrandom[irandom]=(int) (100*(Math.random()));
-           
+           sum=sum+Arrayrandom[irandom];
+           avg=(double)sum/(double)num;
         }
         for(int irandom=0;irandom<Arrayrandom.length; irandom++)
            {
@@ -22,22 +29,53 @@ public class Histagram
            }
         for (int irandom=0; irandom<Count.length; irandom++)
             {
-                System.out.println(irandom+":"+Count[irandom]);
+               System.out.print(irandom+":");
+                  for(int i=0; i<Count[irandom]; i++)
+                  {
+                      System.out.print("#");
+                  }
+               System.out.print("\n");    
             }
-        int irandom=0
-        while(irandom<Count.length)
+        for(int irandom=0;irandom<Arrayrandom.length; irandom++)
         {
-            int hashtag=Count[irandom]
-            if()
+            if(Arrayrandom[irandom]<min)
             {
-                System.out.println(irandom+":")
+                min=Arrayrandom[irandom];
             }
-            else
-            {
-                System.out.println(irandom+":")
-            }
-            irandom++;
         }
-        
+        for(int irandom=0;irandom<Arrayrandom.length; irandom++)
+        {
+            if(Arrayrandom[irandom]>=max)
+            {
+                max=Arrayrandom[irandom];
+            }
+        }
+        for(int irandom=0;irandom<Arrayrandom.length; irandom++)
+        {
+            if(Count[irandom]>moc)
+            {
+                moc=Count[irandom];
+            }
+        }
+        for(int irandom=0;irandom<Arrayrandom.length; irandom++)
+        {
+            if(Count[irandom]==moc)
+            {
+                mst[irandom]=irandom;
+            }
+        }
+        System.out.println("MIN: "+min);
+        System.out.println("MAX: "+max);
+        System.out.println("SUM: "+sum);
+        System.out.println("AVG: "+avg);
+        System.out.print("MST: ");
+        for(int irandom=0;irandom<Count.length; irandom++)
+        {
+            if(mst[irandom]>0)
+            {
+                System.out.print(mst[irandom]);
+            }
+        }
+        System.out.print("\n");
               }
     }
