2022e1a84834f308d7d6ad761e46b07637da3f3f
cmis <cmis@cmis-hs09.(none)>
2015-11-20 11:55:49 +0700

    card
    .

---
+++ CardPoject/Card.java
@@ -0,0 +1,58 @@
+
+/**
+ * Write a description of class Card here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Card
+{
+    //Instance Variables
+    private String[] ranks={"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+    private String[] suits={"♠","♣","♦","♥"};
+    private String rank;
+    private String suit;
+    private String card;
+    int value;
+
+    public Card()
+    {
+        suit =suits[2];
+        rank =ranks[7];
+        card =new String(suit+rank); 
+    }
+    
+    public int setvalue()
+    {
+        int value;
+        if(rank.equals("J"))
+            {
+                value=10;
+            }
+            else if(rank.equals("Q"))
+            {
+                value=10;
+            }
+            else if(rank.equals("K"))
+            {
+                value=10;
+            }
+            else if(rank.equals("A"))
+            {
+                value=11;
+            }
+            else 
+            {
+                int ranks = Integer.parseInt(rank);
+                value=ranks;
+            }
+        return value;
+    }
+    
+    public String toString()
+    {
+        String output=new String();
+        output=card+"\n"+setvalue();
+        return output;
+    }
+}
---
+++ CardPoject/CardDriver.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class CardDriver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CardDriver
+{
+    public static void main(String[] args)
+    {
+        Card card1=new Card();
+        System.out.println(card1);
+    }
+}
