077c6181dec873bd8ab54f522b3476bcf722ccdf
cmis <cmis@cmis-hs02.(none)>
2016-01-15 11:56:28 +0700

    still can't figure out for each loop
    .

--- ClassComposition/MyPod/MyPod.java
+++ ClassComposition/MyPod/MyPod.java
@@ -1,27 +1,29 @@
 public class MyPod
 {
-   private String color;
-   private int memory;
-   private String[] SongLibrary;
-   
-   public MyPod()
-   {
-       color = new String("Green");
-       memory = 16;
-       SongLibrary = new String[]{"Adventure of a Lifetime","Crystalized","Shoot to Thrill"};
-   }
-   
-   public MyPod(String color, int memory)
-   {
-       this.color= new String(color);
-       this.memory=memory;
+    private String color;
+    private int memory;
+    private String[] SongLibrary;
+
+    public MyPod()
+    {
+        color = new String("Green");
+        memory = 16;
+        this.SongLibrary = {"Adventure of a Lifetime","Crystalized","Shoot to Thrill"};
     }
-    
+
+    public MyPod(String color, int memory)
+    {
+        this.color= new String(color);
+        this.memory=memory; 
+    }
+
     public String toString()
     {
         String output=new String();
-        output="MyPod Information\nColor: " + color + "\nMemory Capacity: " + memory + "\nSong Library:\n";
-        for (int i=0;i<SongLibrary.length;i++)
+        output="MyPod Information\nColor: " + color + 
+        "\nMemory Capacity: " + memory + 
+        "\nSong Library:\n";
+        for(int i=0;i<SongLibrary.length;i++)
         {
             System.out.println(SongLibrary[i]);
         }
