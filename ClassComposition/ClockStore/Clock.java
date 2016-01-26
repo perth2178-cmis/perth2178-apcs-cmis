faeb3a4858ce70ccfc984ce9656b964f4916ce17
cmis <cmis@cmis-hs09.(none)>
2016-01-26 11:45:52 +0700

    p2 almost
    .

--- ClassComposition/ClockStore/Clock.java
+++ ClassComposition/ClockStore/Clock.java
@@ -4,6 +4,7 @@ public class Clock
     private int hour;
     private int min;
     private int sec;
+    private String time;
     
     //zero argument
     public Clock()
@@ -19,5 +20,16 @@ public class Clock
         this.hour=hour;
         this.min=min;
         this.sec=sec;
+        time=new String(hour+":"+min+":"+sec);
+    }
+    
+    public void totalSeconds(Clock seconds)
+    {
+        int sechour=0;
+        int secmin=0;
+        int total=0;
+        sechour=hour*3600;
+        secmin=min*60;
+        total=sechour+secmin+sec;
     }
 }//end class Clock
--- ClassComposition/ClockStore/ClockStore.java
+++ ClassComposition/ClockStore/ClockStore.java
@@ -1,20 +1,41 @@
 import java.util.ArrayList;
 public class ClockStore
 {
-    private ArrayList clockInStock = new ArrayList();
-    
+    private Clock[] clockInStock = new Clock[3];
+
     public ClockStore()
     {
         Clock Clock1=new Clock(11,25,35);
         Clock Clock2=new Clock(22,55,30);
         Clock Clock3=new Clock(9,19,15);
-        clockInStock.add(Clock1);
-        clockInStock.add(Clock2);
-        clockInStock.add(Clock3);
+        clockInStock[0]=Clock1;
+        clockInStock[1]=Clock2;
+        clockInStock[2]=Clock3;
     }
-    
-    public int mostSeconds()
+
+    /*public int mostSeconds()
     {
+        int total=0;
+        int newtotal=0;
+        for(int i=0;i<clockInStock.length;i++)
+        {
+            Clock ClockA=clockInStock[i];
+            ClockA.totalSeconds(clockInStock[i]);
+            if(total>newtotal)
+            {
+                total=newtotal;
+            }
+        }
+        return newtotal;
+    }*/
+    
+    public String toString()
+    {
+        String output=new String();
+        for(Clock clock : clockInStock)
+        {
+            output+=clock+"\n";
+        }
+        return output;
     }
 }
- 
