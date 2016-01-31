e8fa20adfec2dc456b9c4deb8992451591d289a7
perth <perth>
2016-02-01 06:38:19 +0700

    home commit
    .

--- ClassComposition/Roster/Roster2.java
+++ ClassComposition/Roster/Roster2.java
@@ -1,10 +1,22 @@
 import java.util.ArrayList;
 public class Roster2
 {
-    private ArrayList myStudents=new ArrayList();
-    
+    public ArrayList myStudents=new ArrayList();
+
     public void addStudent(Student newStudent)
     {
-        
+        myStudents.add(newStudent);
     }
+
+    public void dropStudent(String last)
+    {
+        for(int i=0;i<myStudents.size;i++)
+        {
+            if(myStudent.get(i).equals(last))
+            {
+                myStudents.remove(i);
+            }
+        }	
+    }
+
 }
