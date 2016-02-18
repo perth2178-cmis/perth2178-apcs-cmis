38c67f9c24541e505514460e0e32b038b032733f
cmis <cmis@cmis-hs10.(none)>
2016-02-18 14:31:18 +0700

    working on inheritance 3

---
+++ Inheritance/Inheritance3/SuperHeroDriver.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class SuperHeroDriver
+{
+    public static void main(String[] args)
+    {
+        ArrayList<SuperHero> heroes = new ArrayList();
+        heroes.add(AsteroidMan());
+        heroes.add(FriedEggMan());
+        heroes.add(OnePunchMan());
+    }
+}
