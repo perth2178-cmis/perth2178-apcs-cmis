1280353963a43c4e733effe421c3f017daa9ff45
cmis <cmis@cmis-hs21.(none)>
2016-03-28 09:55:51 +0700

    greenfoot

---
+++ Greenfoot/GameTest/Lobster.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+public class Lobster extends Prey
+{
+    public void act() 
+    {
+        if(Greenfoot.getRandomNumber(100)<10)
+        {
+            move(4);
+            if(getX() <=5||getX()>= getWorld().getWidth()-5)
+            {
+                turn(180);
+            }
+            if(getY() <=5||getY()>= getWorld().getHeight()-5)
+            {
+                turn(180);
+            }
+        }
+        if(Greenfoot.getRandomNumber(100)<10)
+        {
+            turn(Greenfoot.getRandomNumber(90)-45);
+        }
+    }    
+}
---
+++ Greenfoot/GameTest/Prey.java
@@ -0,0 +1,7 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+public abstract class Prey extends Actor
+{
+    public Prey() 
+    {
+    }
+}
